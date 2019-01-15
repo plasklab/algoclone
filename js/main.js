152c62542bad0a68c5f182783313c3e2459a9a36
the96 <aar30px2@gmail.com>
2019-01-15 22:22:00 +0900

    paletteの表示を実装

--- js/main.js
+++ js/main.js
@@ -11,17 +11,23 @@ const RIGHT   = "../img/right.png";
 const LEFT    = "../img/left.png";
 const LSTART  = "../img/l_start.png";
 const LEND    = "../img/l_end.png";
+const BSTART  = "../img/b_start.png";
+const BEND    = "../img/b_end.png";
 const SPEAD   = "../img/spead.png";
 const HEART   = "../img/heart.png";
 const DIA     = "../img/dia.png";
 const CLOVER  = "../img/clover.png";
+const S_SPEAD = "../img/spead_start.png";
+const S_HEART = "../img/heart_start.png";
+const S_DIA   = "../img/dia_start.png";
+const S_CLOVER= "../img/clover_start.png";
 const PLAY    = "../img/play.png";
 
 window.onload = function() {
   game = new Core(WIDTH, HEIGHT);
   game.fps = 60;
-  game.preload(PLAYER, MAP, GOAL, ADVANCE, RIGHT, LEFT, LSTART, LEND,
-    SPEAD, HEART, DIA, CLOVER, PLAY);
+  game.preload(PLAYER, MAP, GOAL, ADVANCE, RIGHT, LEFT, LSTART, LEND, BSTART, BEND,
+    SPEAD, HEART, DIA, CLOVER, S_SPEAD, S_HEART, S_DIA, S_CLOVER, PLAY);
 
   game.onload = function() {
     var main_menu = new MainMenu();
